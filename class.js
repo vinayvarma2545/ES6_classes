88921a53c54fc5e5ce25c39640d960b2ebfc84e2
vinay <vinayvarma2545@gmail.com>
2022-09-20 18:36:41 +0530

    class

---
+++ class.js
@@ -0,0 +1,36 @@
+// class gfg {
+
+//  // Constructor
+//  constructor(name, estd, rank){
+//   this.n = name;
+//   this.e = estd;
+//   this.r = rank;
+//  }
+
+//  // Function
+//  decreaserank(){
+//   this.r -= 1;
+//  }
+// }
+// const geeks = new gfg("geeks", 2009, 43)
+ 
+// geeks.decreaserank();
+ 
+// console.log(geeks.r); //Output 42
+
+
+
+class Abcd {
+    constructor(name,id,marks){
+        this.n=name;
+        this.roll=id;
+        this.result=marks
+    }
+    if
+    Increasemarks(){
+        this.result +=5;
+    }
+}
+    const Final = new Abcd ("vinay",3524,75,)
+    Final.Increasemarks();
+    console.log(Final.result)
